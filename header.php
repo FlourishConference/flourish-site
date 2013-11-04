943e93c351460e7366f891ce058f823b2a3f220a
Matt Dumford <mdumford99@gmail.com>
2013-11-04 16:51:06 -0600

    most of header done

---
+++ header.php
@@ -0,0 +1,21 @@
+<div id="header">
+	<div id="logo-container">
+		<div id="date"><h2>DATE</h2></div>
+		<div id="logo">
+			<a href="./">
+				<img src="" alt="Flourish! logo"/>
+			</a>
+		</div>
+	</div>
+	<div id="navbar">
+		<a href="./about.php">About</a>
+		<a href="./speakers.php">Speakers</a>
+		<a href="./schedule.php">Schedule</a>
+		<a href="./miniExpo.php">Mini Expo</a>
+		<a href="./sponsorship.php">Sponsorship Info</a>
+		<a href="./directions.php">Directions</a>
+		<a href="./contact.php">Contact</a>
+		<a href="./credits.php">Credits</a>
+		<a href="./feedback.php">Feedback</a>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title> Flourish! </title>
+
+	<link rel="stylesheet" href="css/default.css" type="text/css" />
+
+</head>
+
+
+
+<body>
+	<div id="main-container" >
+		<?php
+			include("header.php");
+		?>
+	</div>
+
+</body>
+
+</html>
