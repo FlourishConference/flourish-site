9fd798dba71de62db1361fb5007ae1d86eb18b55
Matt Dumford <mdumford99@gmail.com>
2013-11-13 12:41:33 -0600

    header pretty much done

--- header.php
+++ header.php
@@ -7,15 +7,17 @@
 			</a>
 		</div>
 	</div>
+	<br />
 	<div id="navbar">
-		<a href="./about.php">About</a>
-		<a href="./speakers.php">Speakers</a>
-		<a href="./schedule.php">Schedule</a>
-		<a href="./miniExpo.php">Mini Expo</a>
-		<a href="./sponsorship.php">Sponsorship Info</a>
-		<a href="./directions.php">Directions</a>
-		<a href="./contact.php">Contact</a>
-		<a href="./credits.php">Credits</a>
-		<a href="./feedback.php">Feedback</a>
+		<a href="./about.php" class="navbar-element">About</a>
+		<a href="./speakers.php" class="navbar-element">Speakers</a>
+		<a href="./schedule.php" class="navbar-element">Schedule</a>
+		<a href="./miniExpo.php" class="navbar-element">Mini Expo</a>
+		<a href="./sponsorship.php" class="navbar-element">Sponsorship Info</a>
+		<a href="./directions.php" class="navbar-element">Directions</a>
+		<a href="./contact.php" class="navbar-element">Contact</a>
+		<a href="./credits.php" class="navbar-element">Credits</a>
+		<a href="./feedback.php" class="navbar-element">Feedback</a>
 	</div>
+	<br />
 </div>
